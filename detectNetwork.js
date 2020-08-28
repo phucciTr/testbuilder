2303cad6c55ea5d16a1adfd8935aa9e37ce734e9
Phuc <miabam26@gmail.com>
2020-08-27 21:49:47 -0700

    Completed and passed all tests with green checks for all card networks with all the required prefixes and length. For some reason I could not move on any further than 'step 3' since the console kept telling me that detectNetwork() was NOT invoked with a card number that has the (prefix, length) combination(s): (p: ... l: ..), even though I have already made all of the test cases passed.
    Note that the console kept giving a 'different error messsage' every time I refreshed the html page, even though I did NOT make any modification to the source code on (detectNetwork.js or dectNetwork.test.js). Not sure if I missed anything or this is a bug within the console. Basically any 'error message' for a certain 'prefix, length' that the console put out, I have already mades that 'prefix, length' combination passes.
    
    I submitted a help desk ticket, but haven't gotton a respond just yet. I guess I'll wait until tomorrowm morning. If anyone sees this, please take a look.

--- detectNetwork.js
+++ detectNetwork.js
@@ -1,8 +1,8 @@
-// Given a credit card number, this function should return a string with the 
+// Given a credit card number, this function should return a string with the
 // name of a network, like 'MasterCard' or 'American Express'
 // Example: detectNetwork('343456789012345') should return 'American Express'
 
-// How can you tell one card network from another? Easy! 
+// How can you tell one card network from another? Easy!
 // There are two indicators:
 //   1. The first few numbers (called the prefix)
 //   2. The number of digits in the number (called the length)
@@ -13,7 +13,156 @@ var detectNetwork = function(cardNumber) {
   // The American Express network always starts with a 34 or 37 and is 15 digits long
 
   // Once you've read this, go ahead and try to implement this function, then return to the console.
+
+  let firstDigit = Number(cardNumber[0]);
+  let secondDigit = Number(cardNumber[1]);
+
+  // diner's / maestro
+  if (cardNumber.length === 14) {
+    if (firstDigit === 3 && secondDigit === 8 || secondDigit === 9) {
+      return 'Diner\'s Club';
+
+    // maestro
+    } else {
+      return checkPrefix(cardNumber);
+    }
+
+  // american / maestro
+  } else if (cardNumber.length === 15) {
+    if (firstDigit === 3 && secondDigit === 4 || secondDigit === 7) {
+      return 'American Express';
+
+    // maestro
+    } else {
+      return checkPrefix(cardNumber);
+    }
+
+  // visa / master / discover/ maestro
+    // (length(16) = visa = master = discover = maestro),
+    // (length(19, 13) = visa = discover = maestro),
+  } else if (cardNumber.length === 16 || cardNumber.length === 19 || cardNumber.length === 13) {
+
+    // visa
+    if (firstDigit === 4) {
+      return 'Visa';
+
+    // master
+    } else if (firstDigit === 5 && (secondDigit >= 1 && secondDigit <= 5)) {
+      return 'MasterCard';
+
+    // discover / maestro
+    } else {
+      return checkPrefix(cardNumber);
+    }
+
+  // maestro
+  } else if (cardNumber.length >= 12 && cardNumber.length <= 19) {
+    return checkPrefix(cardNumber);
+  }
 };
 
+
+// Helper function
+var checkPrefix = function(cardNumber) {
+
+  let firstDigit = Number(cardNumber[0]);
+  let secondDigit = Number(cardNumber[1]);
+  let thirdDigit = Number(cardNumber[2]);
+  let fourthDigit = cardNumber[3];
+
+  let prefixes = [];
+  let prefix = '';
+
+  // 6 & 5
+  if (firstDigit === 6 && secondDigit === 5) {
+    return 'Discover';
+
+  // first 4 digits
+  } else {
+    prefixes.push(firstDigit, secondDigit, thirdDigit, fourthDigit);
+    prefix = prefixes.join('');
+
+    if (prefix === '6011') {
+      return 'Discover';
+
+    } else if (prefix === '5018' || prefix === '5020' ||
+               prefix === '5038' || prefix === '6304') {
+      return 'Maestro';
+
+    // first 3 digits
+    } else {
+      prefixes.pop();
+      prefix = Number(prefixes.join(''));
+
+      if (prefix >= 644 && prefix <= 649) {
+        return 'Discover';
+      }
+    }
+  }
+
+};
+
+// Assertion
+var assertEqual = function(actual, expected, testName) {
+  if (actual === expected) {
+    console.log('passed');
+  } else {
+    console.log('failed [' + testName + '] expected "' + expected + '" but got "' + actual + '"');
+  }
+};
+
+// Test Case
+// var actual = detectNetwork('38345678901234');
+// var expected = 'Diner\'s Club';
+// assertEqual(actual, expected, 'prefix = 38, length = 14');
+
+// var actual = detectNetwork('373456789012345');
+// var expected = 'American Express';
+// assertEqual(actual, expected, 'prefix = 37, length = 15');
+
+// var actual = detectNetwork('4123456789012');
+// var expected = 'Visa';
+// assertEqual(actual, expected, 'prefix = 5, length = 16');
+
+// var actual = detectNetwork('5112345678901234');
+// var expected = 'MasterCard';
+// assertEqual(actual, expected, 'prefix = 51, length = 16');
+
+// 644 through 649 as a prefix
+// var actual = detectNetwork('6440685686875986');
+// var expected = 'Discover';
+// assertEqual(actual, expected, 'prefix = 644, length = 16');
+
+// var actual = detectNetwork('6451111111111111');
+// var expected = 'Discover';
+// assertEqual(actual, expected, 'prefix 645, length 16');
+
+// var actual = detectNetwork('6451111111111111862');
+// var expected = 'Discover';
+// assertEqual(actual, expected, 'prefix 646, length 19');
+
+// var actual = detectNetwork('6011075497862824824');
+// var expected = 'Discover';
+// assertEqual(actual, expected, 'prefix 6011, length 19');
+
+// var actual = detectNetwork('6578678652784924');
+// var expected = 'Discover';
+// assertEqual(actual, expected, 'prefix 65, length 16');
+
+// var actual = detectNetwork('501852509212');
+// var expected = 'Maestro';
+// assertEqual(actual, expected, 'prefix 5018, length 12');
+
+// var actual = detectNetwork('5020904789489');
+// var expected = 'Maestro';
+// assertEqual(actual, expected, 'prefix 5020, length 13');
+
+// var actual = detectNetwork('50389593849341');
+// var expected = 'Maestro';
+// assertEqual(actual, expected, 'prefix 5038, length 14');
+
+// var actual = detectNetwork('630495938493419');
+// var expected = 'Maestro';
+// assertEqual(actual, expected, 'prefix 6304, length 15');
 
 
